525653c5f9be0c35f9bbf438de3cf22d7eaa7bac
ancheng <ancheng@uvw.org>
2020-08-14 15:04:58 +0800

    init

--- APMDemo/GodsEye/CPUEye.swift
+++ APMDemo/GodsEye/CPUEye.swift
@@ -1,9 +1,75 @@
 import Foundation
-import MachO
-import AssetsLibrary
 
-public func cpuUsage() {
-    let kern: kern_return_t
-    let taskInfo: task_info_data_t
-    
+public class CPUEye {
+
+    public static let shared = CPUEye()
+
+    public func cpuUsage()-> String {
+        var cpuUsageInfo = ""
+        var cpuInfo: processor_info_array_t!
+        var prevCpuInfo: processor_info_array_t?
+        var numCpuInfo: mach_msg_type_number_t = 0
+        var numPrevCpuInfo: mach_msg_type_number_t = 0
+        var numCPUs: uint = 0
+        let CPUUsageLock: NSLock = NSLock()
+        var usage:Float32 = 0
+
+        let mibKeys: [Int32] = [CTL_HW, HW_NCPU]
+        mibKeys.withUnsafeBufferPointer() { mib in
+            var sizeOfNumCPUs: size_t = MemoryLayout<uint>.size
+            let status = sysctl(processor_info_array_t(mutating: mib.baseAddress), 2, &numCPUs, &sizeOfNumCPUs, nil, 0)
+            if status != 0 {
+                numCPUs = 1
+            }
+        }
+
+        var numCPUsU: natural_t = 0
+        let res: kern_return_t = host_processor_info(mach_host_self(), PROCESSOR_CPU_LOAD_INFO, &numCPUsU, &cpuInfo, &numCpuInfo)
+
+        guard res == KERN_SUCCESS else {
+            print("get cpu usage error!")
+            return cpuUsageInfo
+        }
+
+        CPUUsageLock.lock()
+
+        for i in 0 ..< Int32(numCPUs) {
+            var inUse: Int32
+            var total: Int32
+            if let prevCpuInfo = prevCpuInfo {
+                inUse = cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_USER)]
+                    - prevCpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_USER)]
+                    + cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_SYSTEM)]
+                    - prevCpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_SYSTEM)]
+                    + cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_NICE)]
+                    - prevCpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_NICE)]
+                total = inUse + (cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_IDLE)]
+                    - prevCpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_IDLE)])
+            } else {
+                inUse = cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_USER)]
+                    + cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_SYSTEM)]
+                    + cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_NICE)]
+                total = inUse + cpuInfo[Int(CPU_STATE_MAX * i + CPU_STATE_IDLE)]
+            }
+            let coreInfo = Float(inUse) / Float(total)
+            usage += coreInfo
+            print(String(format: "Core: %u Usage: %f", i, Float(inUse) / Float(total)))
+        }
+        cpuUsageInfo = String(format:"%.2f",100 * Float(usage) / Float(numCPUs))
+        CPUUsageLock.unlock()
+
+        if let prevCpuInfo = prevCpuInfo {
+            let prevCpuInfoSize: size_t = MemoryLayout<integer_t>.stride * Int(numPrevCpuInfo)
+            vm_deallocate(mach_task_self_, vm_address_t(bitPattern: prevCpuInfo), vm_size_t(prevCpuInfoSize))
+        }
+
+        prevCpuInfo = cpuInfo
+        numPrevCpuInfo = numCpuInfo
+
+        cpuInfo = nil
+        numCpuInfo = 0
+
+        return cpuUsageInfo
+    }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # GodsEye
-APM
+## 目录
+
+[参考](#参考)
+
+## 参考
+
+[美团-移动端性能监控方案Hertz](https://tech.meituan.com/2016/12/19/hertz.html)
+
+[微信iOS卡顿监控系统](https://mp.weixin.qq.com/s/M6r7NIk-s8Q-TOaHzXFNAw?)
+
+[YYFPSLabel](https://github.com/ibireme/YYText/blob/master/Demo/YYTextDemo/YYFPSLabel.m)
+
+[ Wedjat（华狄特）](https://github.com/aozhimin/iOS-Monitor-Platform)
+
+[Get CPU Usage Percentage Of Single IOS App In Swift 4.X](https://izziswift.com/get-cpu-usage-percentage-of-single-ios-app-in-swift-4-x/)
+
+[苹果开发者文档 runloop](https://developer.apple.com/documentation/foundation/runloop)
+
